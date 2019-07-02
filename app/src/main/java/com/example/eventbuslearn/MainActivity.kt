d58d7a1e4d5afd2377c353211344f0065f2c56c4
liudongxun <531509569@bnv.com>
2019-07-03 00:00:47 +0800

    保存

--- app/src/main/java/com/example/eventbuslearn/MainActivity.kt
+++ app/src/main/java/com/example/eventbuslearn/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.eventbuslearn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -12,10 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        EventBus.getDefault().register(this)
+        EventBus.getDefault().register(this@MainActivity)
+        mainactivity_btn.setOnClickListener {
+            startActivity(Intent(this@MainActivity,SubscribeActivity::class.java))
+        }
+
     }
-    @Subscribe(threadMode = ThreadMode.MAIN)
+    @Subscribe(threadMode = ThreadMode.POSTING)
     fun getEventBusMessage(eventBusTestBean: EventBusTestBean){
+        Log.d("test","thread111111="+(Looper.getMainLooper()==Looper.myLooper()))
           mainactivity_tv.setText(eventBusTestBean.toString())
+    }
+    @Subscribe(threadMode = ThreadMode.POSTING)
+    fun getEventBusMessage2(eventBusTestBean: EventBusTestBean){
+        Log.d("test","thread222222="+(Looper.getMainLooper()==Looper.myLooper()))
+    }
+    @Subscribe(threadMode = ThreadMode.POSTING)
+    fun getEventBusMessage3(intTest:Int){
+        Log.d("test","thread33333="+(Looper.getMainLooper()==Looper.myLooper()))
     }
 }
--- app/src/main/java/com/example/eventbuslearn/SubscribeActivity.kt
+++ app/src/main/java/com/example/eventbuslearn/SubscribeActivity.kt
@@ -7,8 +7,8 @@ import org.greenrobot.eventbus.EventBus
 class SubscribeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        https://www.jianshu.com/p/e7d5c7bda783
         setContentView(R.layout.activity_subscribe)
-        EventBus.getDefault().post(EventBusTestBean("lidongxun",26))
+   //     EventBus.getDefault().post(EventBusTestBean("lidongxun",26))
+        EventBus.getDefault().post(Int)
     }
 }

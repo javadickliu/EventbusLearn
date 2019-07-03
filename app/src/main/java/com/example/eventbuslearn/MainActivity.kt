c92102aa4fdc6dc9bd48a82f270d0ffc8716f607
dongxunliu <531509569@qq.com>
2019-07-03 19:49:10 +0800

    保存

--- app/src/main/java/com/example/eventbuslearn/MainActivity.kt
+++ app/src/main/java/com/example/eventbuslearn/MainActivity.kt
@@ -15,23 +15,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        EventBus.getDefault().register(this@MainActivity)
-        mainactivity_btn.setOnClickListener {
-            startActivity(Intent(this@MainActivity,SubscribeActivity::class.java))
+
+        mainactivity_post_btn.setOnClickListener {
+            EventBus.getDefault().post(EventBusTestBean("lidongxun",26))
+          //  startActivity(Intent(this@MainActivity,SubscribeActivity::class.java))
+        }
+        mainactivity_subscribe_btn.setOnClickListener {
+            EventBus.getDefault().register(this@MainActivity)
         }
 
     }
-    @Subscribe(threadMode = ThreadMode.POSTING)
+    @Subscribe(threadMode = ThreadMode.POSTING ,priority = 1)
     fun getEventBusMessage(eventBusTestBean: EventBusTestBean){
         Log.d("test","thread111111="+(Looper.getMainLooper()==Looper.myLooper()))
           mainactivity_tv.setText(eventBusTestBean.toString())
     }
-    @Subscribe(threadMode = ThreadMode.POSTING)
+    @Subscribe(threadMode = ThreadMode.POSTING,priority = 2)
     fun getEventBusMessage2(eventBusTestBean: EventBusTestBean){
         Log.d("test","thread222222="+(Looper.getMainLooper()==Looper.myLooper()))
+        EventBus.getDefault().cancelEventDelivery(eventBusTestBean)
     }
-    @Subscribe(threadMode = ThreadMode.POSTING)
-    fun getEventBusMessage3(intTest:Int){
-        Log.d("test","thread33333="+(Looper.getMainLooper()==Looper.myLooper()))
-    }
+//    @Subscribe(threadMode = ThreadMode.POSTING)
+//    fun getEventBusMessage3(intTest:Int){
+//        Log.d("test","thread33333="+(Looper.getMainLooper()==Looper.myLooper()))
+//    }
 }
